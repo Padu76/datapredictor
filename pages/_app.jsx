afb45cd208c2081c62d1cf0d21a91db9f5fffc5c
Andrea Padoan <andrea.padoan@gmail.com>
2025-11-05 23:52:29 +0100

    fix: route legacy /analyze to internal API

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,10 @@
 import '../apps/web/styles/globals.css';
+
+// Import fetch shim on client only
+if (typeof window !== 'undefined') {
+  import('../lib/fetch-shim');
+}
+
 import FloatingQuickActions from '../components/FloatingQuickActions';
 
 export default function App({ Component, pageProps }) {
